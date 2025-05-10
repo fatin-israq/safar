d53199c82ea8c75397edfc21699f04ff55b50da5
fatin-israq <israqq2120@gmail.com>
2025-05-10 16:24:59 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+  },
+  content: ["./index2.html", "./src/css/**/*.css"],
+};
